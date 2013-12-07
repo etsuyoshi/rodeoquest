9bce01cbaf0add9a2595159d679f00cc8deea676
tendo <tuyo1009@gmail.com>
2013-12-07 16:47:15 +0900

    first commit

--- rodeoquest/CoolButton.h
+++ rodeoquest/CoolButton.h
@@ -1,13 +1,17 @@
 //
 //  CoolButton.h
-//  Rodeoquest
+//  CoolButton
 //
-//  Created by 遠藤 豪 on 2013/12/07.
-//  Copyright (c) 2013年 endo.tuyo. All rights reserved.
+//  Created by Brian Moakley on 2/21/13.
+//  Copyright (c) 2013 Razeware. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface CoolButton : UIButton
+
+@property  (nonatomic, assign) CGFloat hue;
+@property  (nonatomic, assign) CGFloat saturation;
+@property  (nonatomic, assign) CGFloat brightness;
 
 @end
